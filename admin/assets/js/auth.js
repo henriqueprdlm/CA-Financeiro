89c8be65e1ef33695b968e8493afdd1166d02988
Henrique Pieri de Lima <henrique070425@gmail.com>
2025-06-17 22:21:54 -0300

    principal admin funcionando

---
+++ admin/assets/js/auth.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", async function () {
+    await Clerk.load();
+
+    if (!Clerk.user) {
+        window.location.href = "/cafinanceiro/admin/login.html";
+    } else {
+        const userButtonDiv = document.getElementById("user-button");
+        Clerk.mountUserButton(userButtonDiv, {
+            afterSignOutUrl: '/cafinanceiro/admin/login.html'
+        });
+
+        const observer = new MutationObserver(() => {
+            if (userButtonDiv) {
+                const header = document.querySelector("header");
+                header.style.display = "flex";
+                header.style.justifyContent = "space-between";
+                header.style.alignItems = "center";
+            }
+        });
+        observer.observe(userButtonDiv, { childList: true, subtree: true });
+    }
+});
